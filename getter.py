e49e8fa0996269b1caf27c39f418907caef428c9
Jimmy Rogala <jimmy@roga.la>
2022-03-24 01:18:59 +0100

    added overtaking

--- getter.py
+++ getter.py
@@ -2,6 +2,7 @@ import time
 import requests as r
 import json
 import pandas as pd
+import numpy as np
 import os
 
 
@@ -32,6 +33,66 @@ def minutesecondsToSeconds(s: str):
         return int(splited[0]) * 360 + float(splited[1]) * 60 + float(splited[2])
 
 
+def _overtaking(df: pd.DataFrame):
+    pivot = df.pivot("lap", "driver_name", "cum_lap_time")
+    # Create deep copy TODO: create empty dataframe
+    overtaken = pivot.copy(deep=True)
+    overtaking = pivot.copy(deep=True)
+    clean_lap = pivot.copy(deep=True)
+    curr_lap = pivot.fillna(np.Infinity)
+    next_lap = pivot.shift(1).fillna(np.Infinity)
+    cols = list(overtaken.columns.copy(deep=True))
+    for driver in cols:
+        overtaken[driver] = np.empty((len(pivot), 0)).tolist()
+        overtaking[driver] = np.empty((len(pivot), 0)).tolist()
+        clean_lap[driver] = True
+        for opponent in cols:
+            if driver != opponent:
+                # Compute if one driver got overtaken during the lap
+                overtaken_opponent_driver_cond = (
+                    curr_lap[driver] < curr_lap[opponent]
+                ) & (next_lap[driver] > next_lap[opponent])
+                # Compute if driver overtake during the lap
+                overtaking_opponent_driver_cond = (
+                    curr_lap[opponent] < curr_lap[driver]
+                ) & (next_lap[opponent] > next_lap[driver])
+                # Clean lap is approximation, if driver is less than 3 seconds behind opponent, then lap is not clean
+                clean_lap_opponent_driver_cond = (
+                    curr_lap[driver] - curr_lap[opponent] > 3
+                ) | (curr_lap[driver] - curr_lap[opponent] < 0)
+                # Add overtaking and overtaken
+                overtaken.loc[overtaken_opponent_driver_cond, driver] = overtaken.loc[overtaken_opponent_driver_cond, driver]\
+                    .apply(lambda x: x + [opponent])
+                overtaking.loc[overtaking_opponent_driver_cond, driver] = overtaking.loc[overtaking_opponent_driver_cond, driver]\
+                    .apply(lambda x: x + [opponent])
+                # Boolean for clean driver
+                clean_lap[driver] = clean_lap[driver] & clean_lap_opponent_driver_cond
+    # Join all dataframe
+    df_indexed = df.set_index(["driver_name", "lap"])
+    df_overtaken = df_indexed.join(
+        overtaken.melt(ignore_index=False)
+        .rename({"value": "overtaking"}, axis=1)
+        .reset_index()
+        .set_index(["driver_name", "lap"]),
+        how="left",
+    )
+    df_overtaking = df_overtaken.join(
+        overtaking.melt(ignore_index=False)
+        .rename({"value": "overtaken_by"}, axis=1)
+        .reset_index()
+        .set_index(["driver_name", "lap"]),
+        how="left",
+    )
+    df_cleanlap = df_overtaking.join(
+        clean_lap.melt(ignore_index=False)
+        .rename({"value": "clean_lap"}, axis=1)
+        .reset_index()
+        .set_index(["driver_name", "lap"]),
+        how="left",
+    )
+    return df_cleanlap.reset_index()
+
+
 def lap(series="f1", year=2021, max_round=99):
     """
     Create CSV files for an entire season.
@@ -41,8 +102,8 @@ def lap(series="f1", year=2021, max_round=99):
     checkandcreatefolder("lap", series, year)
     t = time.time()
     for round in range(1, max_round + 1):
-        if t + 1/4 - time.time() > 0:
-            time.sleep(t + 1/4 - time.time())
+        if t + 1 / 4 - time.time() > 0:
+            time.sleep(t + 1 / 4 - time.time())
         t = time.time()
         rawlaps = r.get(
             f"http://ergast.com/api/{series}/{year}/{round}/laps.json?limit=99999"
@@ -72,6 +133,8 @@ def lap(series="f1", year=2021, max_round=99):
         df["cum_lap_time"] = (
             df.loc[:, ["driver_name", "lap_time"]].groupby("driver_name").cumsum()
         )
+        # Compute overtaking and clean lap
+        df = _overtaking(df)
         df.to_csv(f"data/lap/{series}/{year}/{round}.csv", index=None)
     return round
 
@@ -85,14 +148,9 @@ def pitstop(series="f1", year=2021, max_round=99):
     checkandcreatefolder("pitstop", series, year)
     t = time.time()
     for round in range(1, max_round + 1):
-        if t + 1/4 - time.time() > 0:
-            time.sleep(t + 1/4 - time.time())
+        if t + 1 / 4 - time.time() > 0:
+            time.sleep(t + 1 / 4 - time.time())
         t = time.time()
-        try:
-            laps = pd.read_csv(f"data/lap/{series}/{year}/{round}.csv")
-        except FileNotFoundError:
-            # We have no more lap file so we are at the end
-            break
         rawpitstops = r.get(
             f"http://ergast.com/api/{series}/{year}/{round}/pitstops.json?limit=99999"
         )
@@ -118,26 +176,41 @@ def pitstop(series="f1", year=2021, max_round=99):
                 :, ["driver_name", "lap", "pit_amount", "stop", "pit_time"]
             ]
             # Multi index (driver_name, lap)
-            lapsSorted = laps.set_index(["driver_name", "lap"]).sort_index()
-            pitstopsSorted = pitstops.set_index(["driver_name", "lap"]).sort_index()
-            # Merge both dataset and fill all blank with correct value
-            laps_pitstop = lapsSorted.join(pitstopsSorted)
-            laps_pitstop["pit_time"] = laps_pitstop["pit_time"].fillna(0)
-            laps_pitstop["pit_amount"] = (
-                laps_pitstop["pit_amount"].groupby("driver_name").ffill().fillna(0)
-            ).groupby("driver_name").shift(1, fill_value=0)
-            laps_pitstop["stop"] = laps_pitstop["stop"].fillna(False)
-            # Get tyre lap old
-            laps_pitstop["tyre_lap_old_race"] = laps_pitstop.groupby(["driver_name", "pit_amount"]).cumcount() + 1
-            # Save to CSV
-            laps_pitstop = laps_pitstop.reset_index()
-            laps_pitstop.to_csv(f"data/pitstop/{series}/{year}/{round}.csv", index=None)
+            pitstops.to_csv(f"data/pitstop/{series}/{year}/{round}.csv", index=None)
         else:
             raise Exception("Network Error")
     return round
 
 
+def lap_pistop_merge(series="f1", year=2021, max_round=99):
+    laps = pd.read_csv(f"data/lap/{series}/{year}/{round}.csv")
+    pitstops = pd.read_csv(f"data/pitstop/{series}/{year}/{round}.csv")
+    lapsSorted = laps.set_index(["driver_name", "lap"]).sort_index()
+    pitstopsSorted = pitstops.set_index(["driver_name", "lap"]).sort_index()
+    # Merge both dataset and fill all blank with correct value
+    laps_pitstop = lapsSorted.join(pitstopsSorted)
+    laps_pitstop["pit_time"] = laps_pitstop["pit_time"].fillna(0)
+    laps_pitstop["pit_amount"] = (
+        (laps_pitstop["pit_amount"].groupby("driver_name").ffill().fillna(0))
+        .groupby("driver_name")
+        .shift(1, fill_value=0)
+    )
+    laps_pitstop["stop"] = laps_pitstop["stop"].fillna(False)
+    # Get tyre lap old
+    laps_pitstop["tyre_lap_old_race"] = (
+        laps_pitstop.groupby(["driver_name", "pit_amount"]).cumcount() + 1
+    )
+    # Save to CSV
+    laps_pitstop = laps_pitstop.reset_index()
+    laps_pitstop.to_csv(f"data/lap_pitstops/{series}/{year}/{round}.csv", index=None)
+
+
 if __name__ == "__main__":
     for year in range(2014, 2022):
+        print(f"Working for year {year}")
         lap(year=year)
+        print("Lap Done")
         pitstop(year=year)
+        print("Pit stop Done")
+        lap_pistop_merge(year=year)
+        print("Merge done")
